ec1ca836567a132b28e870dab5095b096c22809c
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-25 11:54:10 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,10 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-#define videos
-vid1 = st.video("https://www.youtube.com/watch?v=cuG7GCXCi48")
-vid2 = st.video("https://www.youtube.com/watch?v=CVHj7Wxhvdo")
-
 #header
 st.title("CV DASHBOARD")
 st.subheader("subheader <3")
@@ -16,8 +12,9 @@ col1, col2 = st.columns(2)
 col1.header("video 1")
 col2.header("video 2")
 
-vid1
-vid2
+#define videos
+vid1 = st.video("https://www.youtube.com/watch?v=cuG7GCXCi48")
+vid2 = st.video("https://www.youtube.com/watch?v=CVHj7Wxhvdo")
 
 #video
 #st.video("https://www.youtube.com/watch?v=PoAeFpUB1hA")
