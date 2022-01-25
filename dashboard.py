82c2b205cb7a9eaf0d49badfd1210f6b4b8a550c
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-25 16:49:39 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -55,7 +55,7 @@ elif add_selectbox == 'feed A':
           
 elif add_selectbox == 'feed B':
           with st.expander("adjust zoom"):
-            num = st.slider("", 1, 5, 3)
+            num = st.slider("", 1, 5, 3, None, None, 2)
           #columns
           col1, col2 = st.columns((3,num))
           with col1:
@@ -67,7 +67,7 @@ elif add_selectbox == 'feed B':
           
 elif add_selectbox == 'feed C':
           with st.expander("adjust zoom"):
-            num = st.slider("", 1, 5, 3)
+            num = st.slider("", 1, 5, 3, None, None, 3)
           #columns
           col1, col2 = st.columns((3,num))
           with col1:
