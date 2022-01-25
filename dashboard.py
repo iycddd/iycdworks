7b5388d8ecb00c36cfdac29ad8a2557cf4751731
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-25 15:39:44 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -19,18 +19,23 @@ add_selectbox = st.sidebar.radio(
   ("main", "feed A", "feed B", "feed C"))
 
 #vidlist
-vidlist = ["https://www.youtube.com/watch?v=CVHj7Wxhvdo"]
+vidlist = ["https://www.youtube.com/watch?v=CVHj7Wxhvdo",
+          "https://www.youtube.com/watch?v=hG5i6XM6x7w",
+          "https://www.youtube.com/watch?v=CVHj7Wxhvdo",
+          "https://www.youtube.com/watch?v=9EJIH8kxTn8",
+          "https://www.youtube.com/watch?v=W9wT8uOjv6A",
+          "https://www.youtube.com/watch?v=cbP2N1BQdYc"]
 
 #contents
 with col1:
   st.header("feed A")
   st.video(vidlist[0])
-  st.video("https://www.youtube.com/watch?v=hG5i6XM6x7w")
+  st.video(vidlist[1])
 with col2:
   st.header("feed B")
-  st.video("https://www.youtube.com/watch?v=CVHj7Wxhvdo")
-  st.video("https://www.youtube.com/watch?v=9EJIH8kxTn8")
+  st.video(vidlist[2])
+  st.video(vidlist[3])
 with col3:
   st.header("feed C")
-  st.video("https://www.youtube.com/watch?v=W9wT8uOjv6A")
-  st.video("https://www.youtube.com/watch?v=cbP2N1BQdYc")
+  st.video(vidlist[4])
+  st.video(vidlist[5])
