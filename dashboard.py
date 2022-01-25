ca0647187ca8a489ed77e71791aad617f48e97c7
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-25 14:08:44 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,14 +8,20 @@ st.title("CV DASHBOARD")
 st.subheader("subheader <3")
 
 #columns
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 #col 1
 with col1:
-  st.header("video 1")
+  st.header("feed A")
   st.video("https://www.youtube.com/watch?v=cuG7GCXCi48")
+  st.video("https://www.youtube.com/watch?v=hG5i6XM6x7w")
   
 #col 2
 with col2:
-  st.header("video 2")
+  st.header("feed B")
   st.video("https://www.youtube.com/watch?v=CVHj7Wxhvdo")
+
+#col 3
+with col2:
+  st.header("feed C")
+  st.video("https://www.youtube.com/watch?v=W9wT8uOjv6A")
