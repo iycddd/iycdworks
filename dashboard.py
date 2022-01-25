4bc57cd220533c9cff79d11aa42cb38a3e229abd
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-25 14:06:47 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -13,11 +13,9 @@ col1, col2 = st.columns(2)
 #col 1
 with col1:
   st.header("video 1")
-  st.image("https://www.youtube.com/watch?v=cuG7GCXCi48")
+  st.video("https://www.youtube.com/watch?v=cuG7GCXCi48")
   
 #col 2
 with col2:
   st.header("video 2")
-  st.image("https://www.youtube.com/watch?v=CVHj7Wxhvdo")
-
-st.image("https://www.youtube.com/watch?v=CVHj7Wxhvdo")
+  st.video("https://www.youtube.com/watch?v=CVHj7Wxhvdo")
