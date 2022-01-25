0b1319656401729a29d987306e620f95c33f3de8
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-25 14:10:18 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+#widen page
+st.set_page_config(layout="wide")
+
 #header
 st.title("CV DASHBOARD")
 st.subheader("subheader <3")
@@ -10,18 +13,14 @@ st.subheader("subheader <3")
 #columns
 col1, col2, col3 = st.columns(3)
 
-#col 1
+#contents
 with col1:
   st.header("feed A")
   st.video("https://www.youtube.com/watch?v=cuG7GCXCi48")
   st.video("https://www.youtube.com/watch?v=hG5i6XM6x7w")
-  
-#col 2
 with col2:
   st.header("feed B")
   st.video("https://www.youtube.com/watch?v=CVHj7Wxhvdo")
-
-#col 3
 with col3:
   st.header("feed C")
   st.video("https://www.youtube.com/watch?v=W9wT8uOjv6A")
