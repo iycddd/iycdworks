2acefaa837671d9fc34f7cf5e33c382f6f5249fa
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-25 11:37:47 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,15 +1,13 @@
+#import libraries
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
+#header
 st.title("CV DASHBOARD")
+st.subheader("subheader <3")
 
-st.markdown(' ## Key Metrics')
-
-col1, col2, col3 = st.columns(3)
-col1.metric(label = "SPDR S&P 500", value = '%.2f'
-
-st.markdown('## Detailed Charts')
-
-chart1, chart2 = st.columns(2)
