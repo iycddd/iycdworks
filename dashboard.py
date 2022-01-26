1112082a530c000743376c12f3a93626e3fc8d08
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-26 17:13:25 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -152,12 +152,8 @@ def data():
         def get_data():
            return []
 
-        user_id = st.text_input("User ID")
-        foo = st.slider("foo", 0, 100)
-        bar = st.slider("bar", 0, 100)
-
         if st.button("Add row"):
-           get_data().append({"UserID": user_id, "foo": foo, "bar": bar})
+           get_data().append({"Spotted": spotted})
 
         st.write(pd.DataFrame(get_data()))
             
