eaf197f0014fee83cf1448d38bc401c3c9492bfe
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-27 16:40:37 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,22 +8,7 @@ import pandas as pd
 
 ###
 
-password = st.sidebar.text_input("Password:", value="")
 
-# select our text input field and make it into a password input
-js = "el = document.querySelectorAll('.sidebar-content input')[0]; el.type = 'password';"
-
-# passing js code to the onerror handler of an img tag with no src
-# triggers an error and allows automatically running our code
-html = f'<img src onerror="{js}">'
-
-# in contrast to st.write, this seems to allow passing javascript
-div = Div(text=html)
-st.bokeh_chart(div)
-
-if password != os.environ["PASSWORD"]:
-    st.error("the password you entered is incorrect")
-    #return
 
 ###
 
