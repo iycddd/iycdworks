ad5b9d78fc732914006199a00454aea97ed8114b
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-27 16:36:30 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,52 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import streamlit_authenticator as stauth
+
+###
+
+def is_authenticated(password):
+    return password == "admin"
+
+
+def generate_login_block():
+    block1 = st.empty()
+    block2 = st.empty()
+
+    return block1, block2
+
+
+def clean_blocks(blocks):
+    for block in blocks:
+        block.empty()
+
+
+def login(blocks):
+    blocks[0].markdown("""
+            <style>
+                input {
+                    -webkit-text-security: disc;
+                }
+            </style>
+        """, unsafe_allow_html=True)
+
+    return blocks[1].text_input('Password')
+
+
+def main():
+    st.header('Hello')
+    st.balloons()
+
+
+login_blocks = generate_login_block()
+password = login(login_blocks)
+
+if is_authenticated(password):
+    clean_blocks(login_blocks)
+    main()
+elif password:
+    st.info("Please enter a valid password")
+
+###
 
 #widen page
 st.set_page_config(layout="wide")
