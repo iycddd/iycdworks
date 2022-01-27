cf444ff8d0becdd238c51549946ff9f3067e06b6
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-27 17:31:05 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,6 +5,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import streamlit_authenticator as stauth
 
 ###
 
@@ -16,6 +17,34 @@ from streamlit_webrtc import (
 
 #widen page
 st.set_page_config(layout="wide")
+
+#authentication system
+names = ['tim']
+usernames = ['iycddd']
+passwords = ['123']
+
+hashed_passwords = stauth.hasher(passwords).generate()
+
+authenticator = stauth.authenticate(names,usernames,hashed_passwords,
+    'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+
+name, authentication_status = authenticator.login('Login','main')
+
+if authentication_status:
+    st.write('Welcome *%s*' % (name))
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+    
+if st.session_state['authentication_status']:
+    st.write('Welcome *%s*' % (st.session_state['name']))
+    st.title('Some content')
+elif st.session_state['authentication_status'] == False:
+    st.error('Username/password is incorrect')
+elif st.session_state['authentication_status'] == None:
+    st.warning('Please enter your username and password')
 
 #sidebar
 add_selectbox = st.sidebar.radio(
