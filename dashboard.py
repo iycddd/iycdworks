08bcd25fe56cbb73c6c576d7d7e75c238610b313
iycddd <98306965+iycddd@users.noreply.github.com>
2022-01-27 17:34:49 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -49,150 +49,150 @@ def authenticate():
 
 authenticate()
 
-    #sidebar
-    add_selectbox = st.sidebar.radio(
-      "menu",
-      ("main", "feed A", "feed B", "feed C",
-       #"data"
-      ))
+#sidebar
+add_selectbox = st.sidebar.radio(
+  "menu",
+  ("main", "feed A", "feed B", "feed C",
+   #"data"
+  ))
 
-    #############
-    ## vidlist ##
-    #############
+#############
+## vidlist ##
+#############
 
-    vidlist = ["https://www.youtube.com/watch?v=5qap5aO4i9A",
-              "https://www.youtube.com/watch?v=hG5i6XM6x7w",
-              "https://www.youtube.com/watch?v=XLxToJ4mauY",
-              "https://www.youtube.com/watch?v=9EJIH8kxTn8",
-              "https://www.youtube.com/watch?v=W9wT8uOjv6A",
-              "https://www.youtube.com/watch?v=cbP2N1BQdYc"]
-
-
-    #############
-    ## display ##
-    #############
-
-    def main():
-      if add_selectbox == 'main':
-        mainpage()
-      elif add_selectbox == 'feed A':
-       feedA()
-      elif add_selectbox == 'feed B':
-        feedB()
-      elif add_selectbox == 'feed C':
-        feedC()
-    #  elif add_selectbox == 'data':
-    #    data()
-
-    def mainpage():
-
-              #layout columns
-              firstcol, blankcol, seccol, blankcol2, thirdcol = st.columns((3,2,6,2,3))
-
-              #header column
-              with firstcol:
-                st.title("CV DASHBOARD")
-                st.subheader("next gen fusion 👌")
-
-              #sightings input column
-              with thirdcol:
-                st.title(" ")
-                spotted = st.text_input("TARGET SIGHTED:", "clear")
-                st.write("input 'clear' to clear")
-
-              #sightings display column
-              with seccol:
-                st.title(" ")
-                if spotted == "clear":
-                  st.empty()
-                else:
-                  warn = st.write("⚠️ ", spotted, "has been spotted ⚠️")
-                  st.warning("targets sighted!")
-
-              #columns
-              col1, col2, col3 = st.columns(3)
-
-              #vidcontent
-              with col1:
-                st.header("feed A")
-                st.video(vidlist[0])
-                st.video(vidlist[1])
-              with col2:
-                st.header("feed B")
-                st.video(vidlist[2])
-                st.video(vidlist[3])
-              with col3:
-                st.header("feed C")
-                st.video(vidlist[4])
-                st.video(vidlist[5])
-
-    ## feed A ##
-
-    def feedA():
-
-              #zoomslider
-              with st.expander("adjust zoom"):
-                slideval = st.slider("", 1, 5, 3, None, None, 2)  
-
-              #columns
-              col1, col2 = st.columns((3,slideval))
-
-              #vidcontent
-              with col1:
-                st.header("feed A")    
-                st.video(vidlist[0])
-              with col2:
-                st.header("anafi")
-                st.video(vidlist[1])
+vidlist = ["https://www.youtube.com/watch?v=5qap5aO4i9A",
+          "https://www.youtube.com/watch?v=hG5i6XM6x7w",
+          "https://www.youtube.com/watch?v=XLxToJ4mauY",
+          "https://www.youtube.com/watch?v=9EJIH8kxTn8",
+          "https://www.youtube.com/watch?v=W9wT8uOjv6A",
+          "https://www.youtube.com/watch?v=cbP2N1BQdYc"]
 
 
-    ## feed B ##
+#############
+## display ##
+#############
 
-    def feedB():
+def main():
+  if add_selectbox == 'main':
+    mainpage()
+  elif add_selectbox == 'feed A':
+   feedA()
+  elif add_selectbox == 'feed B':
+    feedB()
+  elif add_selectbox == 'feed C':
+    feedC()
+#  elif add_selectbox == 'data':
+#    data()
 
-              #zoomslider
-              with st.expander("adjust zoom"):
-                slideval = st.slider("", 1, 5, 3, None, None, 2)  
+def mainpage():
 
-              #columns
-              col1, col2 = st.columns((3,slideval))
+          #layout columns
+          firstcol, blankcol, seccol, blankcol2, thirdcol = st.columns((3,2,6,2,3))
 
-              #content
-              with col1:
-                st.header("feed B")
-                st.video(vidlist[2])
+          #header column
+          with firstcol:
+            st.title("CV DASHBOARD")
+            st.subheader("next gen fusion 👌")
 
-              with col2:
-                st.header("anafi")
-                st.video(vidlist[3])
+          #sightings input column
+          with thirdcol:
+            st.title(" ")
+            spotted = st.text_input("TARGET SIGHTED:", "clear")
+            st.write("input 'clear' to clear")
+
+          #sightings display column
+          with seccol:
+            st.title(" ")
+            if spotted == "clear":
+              st.empty()
+            else:
+              warn = st.write("⚠️ ", spotted, "has been spotted ⚠️")
+              st.warning("targets sighted!")
+
+          #columns
+          col1, col2, col3 = st.columns(3)
+
+          #vidcontent
+          with col1:
+            st.header("feed A")
+            st.video(vidlist[0])
+            st.video(vidlist[1])
+          with col2:
+            st.header("feed B")
+            st.video(vidlist[2])
+            st.video(vidlist[3])
+          with col3:
+            st.header("feed C")
+            st.video(vidlist[4])
+            st.video(vidlist[5])
+
+## feed A ##
+
+def feedA():
+
+          #zoomslider
+          with st.expander("adjust zoom"):
+            slideval = st.slider("", 1, 5, 3, None, None, 2)  
+
+          #columns
+          col1, col2 = st.columns((3,slideval))
+
+          #vidcontent
+          with col1:
+            st.header("feed A")    
+            st.video(vidlist[0])
+          with col2:
+            st.header("anafi")
+            st.video(vidlist[1])
 
 
-    ## feed C ##
+## feed B ##
 
-    def feedC():
+def feedB():
 
-              #zoomslider
-              with st.expander("adjust zoom"):
-                slideval = st.slider("", 1, 5, 3, None, None, 2)  
+          #zoomslider
+          with st.expander("adjust zoom"):
+            slideval = st.slider("", 1, 5, 3, None, None, 2)  
 
-              #columns
-              col1, col2 = st.columns((3,slideval))
+          #columns
+          col1, col2 = st.columns((3,slideval))
 
-              #vidcontent
-              with col1:
-                st.header("feed C")
-                st.video(vidlist[4])
+          #content
+          with col1:
+            st.header("feed B")
+            st.video(vidlist[2])
 
-              with col2:
-                st.header("anafi")
-                st.video(vidlist[5])
+          with col2:
+            st.header("anafi")
+            st.video(vidlist[3])
 
-    ## data ##
 
-    def data():
-            @st.cache(allow_output_mutation=True)
-            def get_data():
-               return []
+## feed C ##
+
+def feedC():
+
+          #zoomslider
+          with st.expander("adjust zoom"):
+            slideval = st.slider("", 1, 5, 3, None, None, 2)  
+
+          #columns
+          col1, col2 = st.columns((3,slideval))
+
+          #vidcontent
+          with col1:
+            st.header("feed C")
+            st.video(vidlist[4])
+
+          with col2:
+            st.header("anafi")
+            st.video(vidlist[5])
+
+## data ##
+
+def data():
+        @st.cache(allow_output_mutation=True)
+        def get_data():
+           return []
 
             if st.button("Add row"):
                get_data().append({"Spotted": spotted})
