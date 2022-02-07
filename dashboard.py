2ebd4924332d8a80c818fce892551db8427b888c
iycddd <98306965+iycddd@users.noreply.github.com>
2022-02-07 09:09:13 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -193,7 +193,7 @@ def feedC():
 def data():
 
         df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
-        grid_return = AgGrid(df, editable=True)
+        grid_return = AgGrid(df, editable=True, theme = light)
         new_df = grid_return['data']
 
 
