eecccc9f5c8c34ebfe760a833283ab04eda3bc5b
iycddd <98306965+iycddd@users.noreply.github.com>
2022-02-07 14:50:35 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,8 +10,12 @@ import numpy as np
 import pandas as pd
 import streamlit_authenticator as stauth
 from streamlit_webrtc import webrtc_streamer
+
+
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 from st_aggrid.shared import GridUpdateMode, DataReturnMode, JsCode
+
+import SessionState
 
 #widen page
 st.set_page_config(layout="wide")
@@ -56,7 +60,7 @@ def authenticate():
 add_selectbox = st.sidebar.radio(
   "menu",
   ("main", "feed A", "feed B", "feed C",
-   "data"
+   "data", "data2"
   ))
 
 #############
@@ -86,6 +90,8 @@ def main():
     feedC()
   elif add_selectbox == 'data':
     data()
+  elif add_selectbox == 'data2':
+    data2()
 
 def mainpage():
 
@@ -192,6 +198,35 @@ def feedC():
 
 ## data ##
 
+def data2():
+
+    # https://gist.githubusercontent.com/tvst/036da038ab3e999a64497f42de966a92/raw/f0db274dd4d295ee173b4d52939be5ad55ae058d/SessionState.py
+
+    # Create an empty dataframe
+    data = pd.DataFrame(columns=["Random"])
+    st.text("Original dataframe")
+
+    # with every interaction, the script runs from top to bottom
+    # resulting in the empty dataframe
+    st.dataframe(data) 
+
+    # persist state of dataframe
+    session_state = SessionState.get(df=data)
+
+    # random value to append; could be a num_input widget if you want
+    random_value = np.random.randn()
+
+    if st.button("Append random value"):
+        # update dataframe state
+        session_state.df = session_state.df.append({'Random': random_value}, ignore_index=True)
+        st.text("Updated dataframe")
+        st.dataframe(session_state.df)
+
+    # still empty as state is not persisted
+    st.text("Original dataframe")
+    st.dataframe(data)
+    
+    
 def data():
 
 #        df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
