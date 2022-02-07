6f8e9f035056783bc64163f3be94deafb19f0ae0
iycddd <98306965+iycddd@users.noreply.github.com>
2022-02-07 09:14:36 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -194,12 +194,13 @@ def data():
 
         df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [4, 5, 6]})
         grid_return = AgGrid(df,
-                             editable=True,
-                             gridOptions=gb.build(),
-                             data_return_mode="filtered_and_sorted",
-                             update_mode="no_update",
-                             fit_columns_on_grid_load=True,
-                             theme = light)
+                             #editable=True,
+                             #gridOptions=gb.build(),
+                             #data_return_mode="filtered_and_sorted",
+                             #update_mode="no_update",
+                             #fit_columns_on_grid_load=True,
+                             #theme = light
+                            )
         new_df = grid_return['data']
 
 
