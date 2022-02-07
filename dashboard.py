f1158ff84dee937c272fcfc472fc0c9b46f8d152
iycddd <98306965+iycddd@users.noreply.github.com>
2022-02-07 10:47:55 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -206,43 +206,50 @@ def data():
 #        gridApi.applyTransaction({add: [{}]})
 #        
 
-        def display_table(df: pd.DataFrame) -> AgGrid:
-            # Configure AgGrid options
-            gb = GridOptionsBuilder.from_dataframe(df)
-            gb.configure_selection('single', use_checkbox=True)
+        @st.cache(allow_output_mutation=True)
+        def load_data():
+            df = pd.DataFrame({'fruit':['Apple', 'Banana'], 'unit_price':[50,25], 'num_units':[0,0], 'total_price':[0,0]})
+            return df
 
-            # Custom JS code for interactive rows deletion
-            # For credits SEE: 
-            # https://github.com/PablocFonseca/streamlit-aggrid/blob/1acb526ba43b5aac9c8eb22cc54eeb05696cd84d/examples/example_highlight_change.py#L21
-            # https://ag-grid.zendesk.com/hc/en-us/articles/360020160932-Removing-selected-rows-or-cells-when-Backspace-or-Delete-is-pressed
-            js = JsCode("""
-            function(e) {
-                let api = e.api;        
-                let sel = api.getSelectedRows();
+        loaded_data = load_data()
 
-                api.applyTransaction({remove: sel});
-            };
-            """)
-            gb.configure_grid_options(onRowSelected=js) 
-            return AgGrid(
-                df,
-                gridOptions=gb.build(),
-                # this override the default VALUE_CHANGED
+        num_units = st.slider('Select Number of Units', 0, 130, 1)
+        loaded_data['num_units'] = num_units
+
+        js_calculate_total = JsCode("""
+
+            function (params) {
+
+                 var unit_price = params.data.unit_price;
+                 var num_units = params.data.num_units;
+                 return (unit_price * num_units).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 0});
+
+            }
+
+        """)
+
+        gb = GridOptionsBuilder.from_dataframe(loaded_data) #Infer basic colDefs from dataframe types
+        # Grid Builder configurations
+        gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=False)
+        # Below, the first parameters are all the same as in the load_data dateframe, but the "header_name" is what is displayed and can be anything 
+        gb.configure_column("fruit", header_name='Fruit', pivot=True, sort='asc')
+        gb.configure_column("unit_price", header_name='Unit Price', sort='desc', type=["numericColumn","numberColumnFilter"], valueFormatter="data.estimatednotional_usd.toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 0})", aggFunc='sum') # defines numeric column
+        gb.configure_column("total_price", header_name='Total Price', type=["numericColumn","numberColumnFilter"], editable=False, valueGetter=js_calculate_total) # uses custom value getter for calculated column
+        gridOptions = gb.build()
+
+        grid_response = AgGrid(
+                loaded_data,
+                gridOptions = gridOptions,
+                height=200,
+                width='100%', # how much of the Streamlit page width this grid takes up
                 update_mode=GridUpdateMode.MODEL_CHANGED,
-                # needed for js injection
-                allow_unsafe_jscode=True
-            )
-
-
-        # Define dummy data
-        rng = np.random.default_rng(2021)
-        N_SAMPLES = 100
-        N_FEATURES = 10
-        df = pd.DataFrame(rng.integers(0, N_SAMPLES, size=(N_SAMPLES, N_FEATURES)), columns=list(string.ascii_uppercase[:N_FEATURES]))
-
-        st.info("Select a row to remove it")
-        response = display_table(df)
-        st.write(f"Dataframe shape: {response['data'].shape}")
+                fit_columns_on_grid_load=True, # automatically fits all columns to be displayed in the grid in one view (doesn't always seem to work)
+                allow_unsafe_jscode=True, # Set to True to allow the Javascript code to be injected
+                enable_enterprise_modules=True, # enables right click and fancy features - can add license key as another parameter (license_key='string') if you have one
+                key='select_grid', # stops grid from re-initialising every time the script is run
+                reload_data=True, # allows modifications to loaded_data to update this same grid entity
+                theme='light'
+                )
             
             
 
