8f1bb5f692f89a4f7fbde937af91862b2f2827a9
iycddd <98306965+iycddd@users.noreply.github.com>
2022-02-08 09:49:04 +0800

    new test

--- dashboard.py
+++ dashboard.py
@@ -254,4 +254,47 @@ def data2():
 
     st.write(pd.DataFrame(get_data()))
             
+def data3():
+    
+    def display_table(df: pd.DataFrame) -> AgGrid:
+    # Configure AgGrid options
+    gb = GridOptionsBuilder.from_dataframe(df)
+    gb.configure_selection('single', use_checkbox=True)
+    
+    # Custom JS code for interactive rows deletion
+    # For credits SEE: 
+    # https://github.com/PablocFonseca/streamlit-aggrid/blob/1acb526ba43b5aac9c8eb22cc54eeb05696cd84d/examples/example_highlight_change.py#L21
+    # https://ag-grid.zendesk.com/hc/en-us/articles/360020160932-Removing-selected-rows-or-cells-when-Backspace-or-Delete-is-pressed
+    js = JsCode("""
+    function(e) {
+        let api = e.api;        
+        let sel = api.getSelectedRows();
+        
+        api.applyTransaction({remove: sel});
+    };
+    """)
+    gb.configure_grid_options(onRowSelected=js) 
+    return AgGrid(
+        df,
+        gridOptions=gb.build(),
+        # this override the default VALUE_CHANGED
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        # needed for js injection
+        allow_unsafe_jscode=True
+    )
+
+
+    # Define dummy data
+    rng = np.random.default_rng(2021)
+    N_SAMPLES = 100
+    N_FEATURES = 10
+    df = pd.DataFrame(rng.integers(0, N_SAMPLES, size=(
+        N_SAMPLES, N_FEATURES)), columns=list(string.ascii_uppercase[:N_FEATURES]))
+
+    st.info("Select a row to remove it")
+    response = display_table(df)
+    st.write(f"Dataframe shape: {response['data'].shape}")
+        
+        
+        
 main()
