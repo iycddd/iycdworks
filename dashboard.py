5cfaca2f0a698c470ca873102e553bcd28284cbf
iycddd <98306965+iycddd@users.noreply.github.com>
2022-02-08 09:51:53 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -60,7 +60,7 @@ def authenticate():
 add_selectbox = st.sidebar.radio(
   "menu",
   ("main", "feed A", "feed B", "feed C",
-   "data", "data2"
+   "data", "data2", "data3"
   ))
 
 #############
@@ -92,6 +92,8 @@ def main():
     data()
   elif add_selectbox == 'data2':
     data2()
+  elif add_selectbox == 'data3':
+    data3()
 
 def mainpage():
 
@@ -257,31 +259,31 @@ def data2():
 def data3():
     
     def display_table(df: pd.DataFrame) -> AgGrid:
-    # Configure AgGrid options
-    gb = GridOptionsBuilder.from_dataframe(df)
-    gb.configure_selection('single', use_checkbox=True)
-    
-    # Custom JS code for interactive rows deletion
-    # For credits SEE: 
-    # https://github.com/PablocFonseca/streamlit-aggrid/blob/1acb526ba43b5aac9c8eb22cc54eeb05696cd84d/examples/example_highlight_change.py#L21
-    # https://ag-grid.zendesk.com/hc/en-us/articles/360020160932-Removing-selected-rows-or-cells-when-Backspace-or-Delete-is-pressed
-    js = JsCode("""
-    function(e) {
-        let api = e.api;        
-        let sel = api.getSelectedRows();
-        
-        api.applyTransaction({remove: sel});
-    };
-    """)
-    gb.configure_grid_options(onRowSelected=js) 
-    return AgGrid(
-        df,
-        gridOptions=gb.build(),
-        # this override the default VALUE_CHANGED
-        update_mode=GridUpdateMode.MODEL_CHANGED,
-        # needed for js injection
-        allow_unsafe_jscode=True
-    )
+        # Configure AgGrid options
+        gb = GridOptionsBuilder.from_dataframe(df)
+        gb.configure_selection('single', use_checkbox=True)
+
+        # Custom JS code for interactive rows deletion
+        # For credits SEE: 
+        # https://github.com/PablocFonseca/streamlit-aggrid/blob/1acb526ba43b5aac9c8eb22cc54eeb05696cd84d/examples/example_highlight_change.py#L21
+        # https://ag-grid.zendesk.com/hc/en-us/articles/360020160932-Removing-selected-rows-or-cells-when-Backspace-or-Delete-is-pressed
+        js = JsCode("""
+        function(e) {
+            let api = e.api;        
+            let sel = api.getSelectedRows();
+
+            api.applyTransaction({remove: sel});
+        };
+        """)
+        gb.configure_grid_options(onRowSelected=js) 
+        return AgGrid(
+            df,
+            gridOptions=gb.build(),
+            # this override the default VALUE_CHANGED
+            update_mode=GridUpdateMode.MODEL_CHANGED,
+            # needed for js injection
+            allow_unsafe_jscode=True
+        )
 
 
     # Define dummy data
