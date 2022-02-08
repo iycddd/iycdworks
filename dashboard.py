f744beba27ffa454847d4460513530f5b71dc9c1
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-02-08 21:42:02 +0800

    adfahdfhdsh

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,10 @@
+from st_aggrid.shared import GridUpdateMode, DataReturnMode, JsCode
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+#import chime
+from streamlit_webrtc import webrtc_streamer
+import pandas as pd
+import numpy as np
+import streamlit as st
 showErrorDetails = False
 hideTopBar = True
 
@@ -5,36 +12,27 @@ hideTopBar = True
 ## setup ##
 ###########
 
-import streamlit as st
-import numpy as np
-import pandas as pd
-import streamlit_authenticator as stauth
-from streamlit_webrtc import webrtc_streamer
-import chime
 
-from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
-from st_aggrid.shared import GridUpdateMode, DataReturnMode, JsCode
-
-#widen page
+# widen page
 st.set_page_config(layout="wide")
 
-#sidebar
+# sidebar
 add_selectbox = st.sidebar.radio(
-  "menu",
-  ("main", "feed A", "feed B", "feed C",
-   "data", "data2", "data3"
-  ))
+    "menu",
+    ("main", "feed A", "feed B", "feed C",
+     "data", "data2", "data3"
+     ))
 
 #############
 ## vidlist ##
 #############
 
 vidlist = ["https://www.youtube.com/watch?v=5qap5aO4i9A",
-          "https://www.youtube.com/watch?v=XWq5kBlakcQ",
-          "https://www.youtube.com/watch?v=XLxToJ4mauY",
-          "https://www.youtube.com/watch?v=9EJIH8kxTn8",
-          "https://www.youtube.com/watch?v=W9wT8uOjv6A",
-          "https://www.youtube.com/watch?v=cbP2N1BQdYc"]
+           "https://www.youtube.com/watch?v=XWq5kBlakcQ",
+           "https://www.youtube.com/watch?v=XLxToJ4mauY",
+           "https://www.youtube.com/watch?v=9EJIH8kxTn8",
+           "https://www.youtube.com/watch?v=W9wT8uOjv6A",
+           "https://www.youtube.com/watch?v=cbP2N1BQdYc"]
 
 
 #############
@@ -42,80 +40,83 @@ vidlist = ["https://www.youtube.com/watch?v=5qap5aO4i9A",
 #############
 
 def main():
-  if add_selectbox == 'main':
-    mainpage()
-  elif add_selectbox == 'feed A':
-   feedA()
-  elif add_selectbox == 'feed B':
-    feedB()
-  elif add_selectbox == 'feed C':
-    feedC()
-  elif add_selectbox == 'data':
-    data()
-  elif add_selectbox == 'data2':
-    data2()
-  elif add_selectbox == 'data3':
-    data3()
+    if add_selectbox == 'main':
+        mainpage()
+    elif add_selectbox == 'feed A':
+        feedA()
+    elif add_selectbox == 'feed B':
+        feedB()
+    elif add_selectbox == 'feed C':
+        feedC()
+    elif add_selectbox == 'data':
+        data()
+    elif add_selectbox == 'data2':
+        data2()
+    elif add_selectbox == 'data3':
+        data3()
+
 
 def mainpage():
 
-      #layout columns
-      firstcol, blankcol, seccol, blankcol2, thirdcol = st.columns((3,2,6,2,3))
+    # layout columns
+    firstcol, blankcol, seccol, blankcol2, thirdcol = st.columns(
+        (3, 2, 6, 2, 3))
 
-      #header column
-      with firstcol:
+    # header column
+    with firstcol:
         st.title("CV DASHBOARD")
         st.subheader("next gen fusion 👌")
 
-      #sightings input column
-      with thirdcol:
+    # sightings input column
+    with thirdcol:
         st.title(" ")
         spotted = st.text_input("TARGET SIGHTED:", "clear")
         st.write("input 'clear' to clear")
 
-      #sightings display column
-      with seccol:
+    # sightings display column
+    with seccol:
         st.title(" ")
         if spotted == "clear":
-          st.empty()
+            st.empty()
         else:
-          warn = st.write("⚠️ ", spotted, "has been spotted ⚠️")
-          st.warning("targets sighted!")
-          chime.success()
+            warn = st.write("⚠️ ", spotted, "has been spotted ⚠️")
+            st.warning("targets sighted!")
+            chime.success()
 
-      #columns
-      col1, col2, col3 = st.columns(3)
+    # columns
+    col1, col2, col3 = st.columns(3)
 
-      #vidcontent
-      with col1:
+    # vidcontent
+    with col1:
         st.header("feed A")
         st.video(vidlist[0])
         st.video(vidlist[1])
-      with col2:
+    with col2:
         st.header("feed B")
         st.video(vidlist[2])
         st.video(vidlist[3])
-      with col3:
+    with col3:
         st.header("feed C")
         st.video(vidlist[4])
         st.video(vidlist[5])
 
 ## feed A ##
 
+
 def feedA():
 
-      #zoomslider
-      with st.expander("adjust zoom"):
-        slideval = st.slider("", 1, 5, 3, None, None, 2)  
+    # zoomslider
+    with st.expander("adjust zoom"):
+        slideval = st.slider("", 1, 5, 3, None, None, 2)
 
-      #columns
-      col1, col2 = st.columns((3,slideval))
+    # columns
+    col1, col2 = st.columns((3, slideval))
 
-      #vidcontent
-      with col1:
-        st.header("feed A")    
+    # vidcontent
+    with col1:
+        st.header("feed A")
         st.video(vidlist[0])
-      with col2:
+    with col2:
         st.header("anafi")
         st.video(vidlist[1])
 
@@ -124,19 +125,19 @@ def feedA():
 
 def feedB():
 
-      #zoomslider
-      with st.expander("adjust zoom"):
-        slideval = st.slider("", 1, 5, 3, None, None, 2)  
+    # zoomslider
+    with st.expander("adjust zoom"):
+        slideval = st.slider("", 1, 5, 3, None, None, 2)
 
-      #columns
-      col1, col2 = st.columns((3,slideval))
+    # columns
+    col1, col2 = st.columns((3, slideval))
 
-      #content
-      with col1:
+    # content
+    with col1:
         st.header("feed B")
         st.video(vidlist[2])
 
-      with col2:
+    with col2:
         st.header("anafi")
         st.video(vidlist[3])
 
@@ -145,42 +146,43 @@ def feedB():
 
 def feedC():
 
-      #zoomslider
-      with st.expander("adjust zoom"):
-        slideval = st.slider("", 1, 5, 3, None, None, 2)  
+    # zoomslider
+    with st.expander("adjust zoom"):
+        slideval = st.slider("", 1, 5, 3, None, None, 2)
 
-      #columns
-      col1, col2 = st.columns((3,slideval))
+    # columns
+    col1, col2 = st.columns((3, slideval))
 
-      #vidcontent
-      with col1:
+    # vidcontent
+    with col1:
         st.header("feed C")
         st.video(vidlist[4])
 
-      with col2:
+    with col2:
         st.header("anafi")
         st.video(vidlist[5])
 
 ## data ##
-   
-def data():    
 
-        df = pd.DataFrame({'timestamp': [2120, 2125, 2136],
-                           'vehicle': ['A', 'B', 'A'],
-                           'quantity': ['1', '2', '1']
-                          })
-        grid_return = AgGrid(df,
-                             editable=True,
-                             #gridOptions=gb.build(),
-                             #data_return_mode="filtered_and_sorted",
-                             #update_mode="no_update",
-                             #fit_columns_on_grid_load=True,
-                             allow_unsafe_jscode=True
-                             #theme=light
-                            )
-        new_df = grid_return['data']
-        
-        newrow = JsCode("""
+
+def data():
+
+    st.session_state['testdf'] = pd.DataFrame({'timestamp': [2120, 2125, 2136],
+                                               'vehicle': ['A', 'B', 'A'],
+                                               'quantity': ['1', '2', '1']
+                                               })
+    grid_return = AgGrid(st.session_state['testdf'],
+                         editable=True,
+                         # gridOptions=gb.build(),
+                         # data_return_mode="filtered_and_sorted",
+                         # update_mode="no_update",
+                         # fit_columns_on_grid_load=True,
+                         allow_unsafe_jscode=True
+                         # theme=light
+                         )
+    new_df = grid_return['data']
+
+    newrow = JsCode("""
         function(e) {
             let api = e.api;        
             let sel = api.getSelectedRows();
@@ -188,95 +190,50 @@ def data():
             api.applyTransaction({add: sel});
         };
         """)
-        gb.configure_grid_options(onRowSelected=js) 
-           
-        if st.button('add'):
-            newrow
-        
-def data2():
+    gb.configure_grid_options(onRowSelected=js)
 
-    @st.cache(allow_output_mutation=True)
-    def get_data():
-       return []
-      
+    if st.button('add'):
+        newrow
+
+
+def data2():
     # Define dummy data
-    df = pd.DataFrame({'timestamp': [],
-                           'vehicle': [],
-                           'quantity': []
-                          })
+    if not 'testdf' in st.session_state:
+        df = st.session_state['testdf'] = pd.DataFrame({'timestamp': [],
+                                                        'vehicle': [],
+                                                        'quantity': []
+                                                        })
+    df = st.session_state['testdf']
 
     col1 = st.text_input("Timestamp")
     col2 = st.text_input("Detected")
     col3 = st.text_input("Quantity")
-    
+
     def addrow():
-       get_data().append({"Timestamp": col1, "Detected": col2, "Quantity": col3})
-       pd.DataFrame().append({"timestamp": [col1], "vehicle": [col2], "quantity": [col3]})
-        
-    
+        st.session_state['testdf'] = df.append(
+            {"timestamp": col1, "vehicle": col2, "quantity": col3}, ignore_index=True)
+
+    def delrow():
+        st.session_state['testdf'] = df.drop(
+            index=st.session_state['testdf'].iloc[-1].name)
+
     if st.button("Add row"):
-       addrow()
+        addrow()
+    if st.button("Remove row"):
+        delrow()
 
-    st.write(pd.DataFrame(get_data()))
-            
-      
-      
-    def display_table(df: pd.DataFrame) -> AgGrid:
-        # Configure AgGrid options
-        gb = GridOptionsBuilder.from_dataframe(df)
-        gb.configure_selection('single', use_checkbox=True)
+    st.write(st.session_state['testdf'])
 
-        # Custom JS code for interactive rows deletion
-        # For credits SEE: 
-        # https://github.com/PablocFonseca/streamlit-aggrid/blob/1acb526ba43b5aac9c8eb22cc54eeb05696cd84d/examples/example_highlight_change.py#L21
-        # https://ag-grid.zendesk.com/hc/en-us/articles/360020160932-Removing-selected-rows-or-cells-when-Backspace-or-Delete-is-pressed
-        js = JsCode("""
-        
-        function(e) {
-        
-            let api = e.api;        
-            let sel = api.getSelectedRows();
-            api.applyTransaction({add: sel});
-            
-            alert(4);
-            
-            
-        };
-     
-        """)
-        gb.configure_grid_options(onRowSelected=js) 
-        
-        
-        
-        return AgGrid(
-            df,
-            gridOptions=gb.build(),
-          
-            # this override the default VALUE_CHANGED
-            update_mode=GridUpdateMode.MODEL_CHANGED,
-            # needed for js injection
-            allow_unsafe_jscode=True
-        )
-    
 
-   
-
-    st.info("Select a row to remove it")
-    response = display_table(df)
-    st.write(f"Dataframe shape: {response['data'].shape}")      
-      
-      
-      
-      
 def data3():
-    
+
     def display_table(df: pd.DataFrame) -> AgGrid:
         # Configure AgGrid options
         gb = GridOptionsBuilder.from_dataframe(df)
         gb.configure_selection('single', use_checkbox=True)
 
         # Custom JS code for interactive rows deletion
-        # For credits SEE: 
+        # For credits SEE:
         # https://github.com/PablocFonseca/streamlit-aggrid/blob/1acb526ba43b5aac9c8eb22cc54eeb05696cd84d/examples/example_highlight_change.py#L21
         # https://ag-grid.zendesk.com/hc/en-us/articles/360020160932-Removing-selected-rows-or-cells-when-Backspace-or-Delete-is-pressed
         js = JsCode("""
@@ -294,32 +251,27 @@ def data3():
         };
      
         """)
-        gb.configure_grid_options(onRowSelected=js) 
-        
-        
-        
+        gb.configure_grid_options(onRowSelected=js)
+
         return AgGrid(
             df,
             gridOptions=gb.build(),
-          
+
             # this override the default VALUE_CHANGED
             update_mode=GridUpdateMode.MODEL_CHANGED,
             # needed for js injection
             allow_unsafe_jscode=True
         )
-    
 
     # Define dummy data
     df = pd.DataFrame({'timestamp': [2120, 2125, 2136],
-                           'vehicle': ['A', 'B', 'A'],
-                           'quantity': ['1', '2', '1']
-                          })
+                       'vehicle': ['A', 'B', 'A'],
+                       'quantity': ['1', '2', '1']
+                       })
 
     st.info("Select a row to remove it")
     response = display_table(df)
     st.write(f"Dataframe shape: {response['data'].shape}")
-     
-        
-      
-        
+
+
 main()
